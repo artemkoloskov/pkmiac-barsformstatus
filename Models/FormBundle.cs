82b414596f3c153040d8119fc1216d0ae82d53eb
Артем Евгеньевич Колосков адм <admkae@MIAC.local>
2020-09-29 22:51:14 +1000

    Добавьте файлы проекта.

---
+++ Models/FormBundle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PKMIAC.BARSFormStatus.Models
+{
+	public partial class FormBundle
+	{
+		public FormBundle()
+		{
+			ReportPeriodComponent = new HashSet<ReportPeriodComponent>();
+		}
+
+		public string Code { get; set; }
+		public bool? Notused { get; set; }
+		public Guid? Id { get; set; }
+		public string Name { get; set; }
+
+		public virtual ICollection<ReportPeriodComponent> ReportPeriodComponent { get; set; }
+	}
+}
---
+++ Models/ReportSubmitChain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PKMIAC.BARSFormStatus.Models
+{
+	public partial class ReportSubmitChain
+	{
+		public ReportSubmitChain()
+		{
+			Komponentotchetnogop = new HashSet<ReportPeriodComponent>();
+		}
+
+		public string Code { get; set; }
+		public bool? Neispolzuetsya { get; set; }
+		public Guid? Id { get; set; }
+		public string Name { get; set; }
+
+		public virtual ICollection<ReportPeriodComponent> Komponentotchetnogop { get; set; }
+	}
+}
